29e885607dd9ccad686ec62ee7b6bb45cedcd51f
Ignas <89981814+IgnasValiukas@users.noreply.github.com>
2025-02-17 08:51:10 +0200

    Add files via upload

---
+++ uzdt_1_3.py
@@ -0,0 +1,12 @@
+# Programoje išbandyti daugiau string funkcijų:
+# upper()
+# casefold()
+# capitalize()
+# count()
+# find() ir t.t.
+line = "I love apples, apple are my favorite fruit"
+
+print("Upper:", line.upper(), )
+print("Casefold:", line.casefold())
+print("Capitalize:", line.capitalize())
+print("Find:", line.find("love"), "Count:", line.count("apple"))
