4175cb08d4c23662443b233bd3b78a122f215999
Ignas <89981814+IgnasValiukas@users.noreply.github.com>
2025-02-20 16:45:31 +0200

    Add files via upload

---
+++ moduliu_importai/Uzduotis2_moduliai/main.py
@@ -0,0 +1,10 @@
+# Importuoti modulį modulis
+import modulis
+from modulis import kintamasis, funkcija
+
+# Atspausdinti importuotą kintamąjį kintamasis
+print(kintamasis)
+# Paleisti importuotą funkciją funkcija()
+funkcija()
+# Inicijuoti importuotos klasės Objektas() objektą
+objektas = modulis.Objektas("Čia yra klasė")
---
+++ moduliu_importai/Uzduotis2_moduliai/modulis.py
@@ -0,0 +1,14 @@
+# Sukurti klasę Objektas, kurį iniciavus atspausdina „Čia yra klasė“.
+class Objektas:
+    def __init__(self, tekstas):
+        self.tekstas = tekstas
+        print(tekstas)
+
+
+# Kintamajam kintamasis priskirti reikšmę „Čia yra kintamasis“
+kintamasis = "Čia yra kintamasis"
+
+
+# Sukurti funkciją funkcija(), kuri atspausdina „Čia yra funkcija“.
+def funkcija():
+    print("Čia yra funkcija")
