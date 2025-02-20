893c3abd46771e438527b17a4b32c46ee4755a31
Ignas <89981814+IgnasValiukas@users.noreply.github.com>
2025-02-20 16:45:52 +0200

    Add files via upload

---
+++ moduliu_importai/mokymas/main.py
@@ -0,0 +1,7 @@
+from modules.python_kursas import PythonKursas, Kursas
+
+kursas1 = Kursas(".NET", "Stasys", "454h")
+kursas2 = PythonKursas("Python", "Marius", "545h")
+
+kursas1.destyti()
+kursas2.destyti()
